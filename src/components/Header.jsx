d3475498a605aa765aee630d38814ba915d29a4c
Ashish Verma <ashishverma510207@gmail.com>
2023-12-25 01:52:48 +0530

    done with the frontend

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,11 @@ const Header = () => {
     <>
         <header className='bg-[#2874f0] '>
             <div className='max-w-screen-xl mx-auto py-2 flex items-center justify-center'>
-                <img src='https://logos-world.net/wp-content/uploads/2020/11/Flipkart-Logo.png' className='w-1/12 mx-4 text-white' alt='logo' />
-                <form className='w-4/12'>   
+                <div className='flex flex-col mx-4'>
+                    <img width={80} src='public/flipkart-plus_8d85f4.png' className='text-white' alt='logo' />
+                    <p className='text-white italic flex font-semibold text-sm  justify-center'>Explore <span className='text-yellow-500 font-semibold'>Plus </span><img className='w-4 mr-1 h-4' src='https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/plus_aef861.png'/></p>
+                </div>
+                {/* <form className='w-4/12'>   
                     <div className="relative shadow">
                         <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                             <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
@@ -16,15 +19,34 @@ const Header = () => {
                         <input type="search" id="default-search" className="block rounded w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 bg-gray-50" placeholder="Search Laptops..." required />
                         <button type="submit" className="text-white absolute end-2.5 bottom-2.5 rounded bg-blue-700 hover:bg-blue-800 outline-none  font-medium  text-sm px-4 py-2 ">Search</button>
                     </div>
-                </form>
-                <button type="button" class="text-blue-800 bg-white focus:outline-none  text-lg font-semibold text-md px-10 py-2 my-auto rounded text-center inline-flex items-center mx-4 mb-2">Login</button>
-                <a href='#' className='text-white mx-2 text-lg'>Become a Seller</a>
+                </form> */}
+
                 
-<button id="dropdownHoverButton" data-dropdown-toggle="dropdownHover" data-dropdown-trigger="hover" className="mx-4 text-white focus:outline-none font-medium text-lg px-5 py-2.5 text-center inline-flex items-center" type="button">More <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+<form className='w-1/3'>
+    <div className="flex">
+        <div className="relative w-full">
+            <input type="search" id="search-dropdown" class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg rounded-s-gray-100 rounded border border-gray-300 " placeholder="Search laptops..." required />
+            <button type="submit" className="absolute top-0 end-0 p-2.5 h-full text-sm font-medium text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 ">
+            <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+            </svg>
+            </button>
+        </div>
+    </div>
+</form>
+
+
+
+
+
+<button type="button" class="text-blue-800 bg-white focus:outline-none  text-lg font-semibold text-md px-10 py-1.5 my-auto rounded text-center inline-flex items-center mx-4">Login</button>
+<a href='#' className='text-white mx-2 text-lg font-medium'>Become a Seller</a>
+                
+<button id="dropdownHoverButton" data-dropdown-toggle="dropdownHover" data-dropdown-trigger="hover" className="mx-4 text-white focus:outline-none font-medium text-lg px-5 py-2.5 text-center inline-flex items-center" type="button">More 
+<svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
 </svg>
 </button>
-
 <div id="dropdownHover" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-50 dark:bg-gray-700">
     <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownHoverButton">
       <li><a href="#" class="block px-4 py-2 hover:bg-gray-100 ">Notification Preferences</a></li><hr />
--- src/components/LaptopCards.jsx
+++ src/components/LaptopCards.jsx
@@ -9,33 +9,33 @@ const LaptopCards = () => {
     <>  
       {laptopList.map((laptop)=>{
         return (
-          <div class="flex flex-col items-center bg-white border border-gray-200  shadow md:flex-row ">
+          <div class="flex flex-col items-center bg-white border justify-between border-gray-200  shadow md:flex-row ">
             <img class="object-contain w-full md:w-1/3 min-h-72 p-4" src={laptop.img_link} alt="photo" />
             <div class="flex flex-col justify-between p-4 ml-2 w-full hover:bg-gray-50 ">
-            <h5 class="text-2xl font-semibold tracking-tight text-gray-900 ">{laptop.name}</h5>
-            <p class=" my-2 text-gray-700 font-medium">
-            {laptop.rating?
-            <span className=" text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600  font-medium rounded text-sm px-2 py-1 text-center mr-2">{laptop.rating} <i className="fa-solid fa-star"></i></span>
-              :''
-            }
-            {/* <span className=" text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600  font-medium rounded text-sm px-4 py-1.5 text-center mr-2">{laptop.rating} <i className="fa-solid fa-star"></i></span> */}
+              <h5 class="text-2xl font-semibold tracking-tight text-gray-900 ">{laptop.name}</h5>
+              <p class=" my-2 text-gray-700 font-medium">
+                {laptop.rating?
+                <span className=" text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600  font-medium rounded text-sm px-2 py-1 text-center mr-2">{laptop.rating} <i className="fa-solid fa-star"></i></span>
+                :''
+              }
+              {/* <span className=" text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600  font-medium rounded text-sm px-4 py-1.5 text-center mr-2">{laptop.rating} <i className="fa-solid fa-star"></i></span> */}
               {laptop.no_of_ratings?laptop.no_of_ratings:0} Ratings & {laptop.no_of_reviews?laptop.no_of_reviews:'No'} Reviews
-            </p>
-            <ul class="max-w-md text-gray-500 list-disc list-inside ">
-              <li><span className='text-black'>{laptop.processor}</span></li>
-              <li><span className='text-black'>{laptop.ram} RAM</span></li>
-              <li><span className='text-black'>{laptop.os} Operating System</span></li>
-              <li><span className='text-black'>{laptop.storage} Storage</span></li>
-              <li><span className='text-black'>{laptop.display*2.54} cm ({laptop.display} Inch) Display</span></li>
-              <li><span className='text-black'>1 Year Onsite Warranty</span></li>
-            </ul>
-            <div className=' my-2 flex justify-between items-center'>
-            <div>
-              <p className='font-semibold text-2xl'>₹{laptop.price/10}</p>
-              <p><s className='text-gray-600 mr-1'>₹{laptop.price}</s><span className='text-green-500 font-bold'>10% off</span></p>
-            </div>
-            <button type="button" className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br  shadow-lg shadow-blue-500/50  font-medium rounded-lg text-sm px-5 py-2.5 text-center">Buy now</button>
-            </div>
+              </p>
+              <ul class="max-w-md text-gray-500 list-disc list-inside ">
+                <li><span className='text-black'>{laptop.processor}</span></li>
+                <li><span className='text-black'>{laptop.ram} RAM</span></li>
+                <li><span className='text-black'>{laptop.os} Operating System</span></li>
+                <li><span className='text-black'>{laptop.storage} Storage</span></li>
+                <li><span className='text-black'>{laptop.display*2.54} cm ({laptop.display} Inch) Display</span></li>
+                <li><span className='text-black'>1 Year Onsite Warranty</span></li>
+              </ul>
+              <div className=' my-2 flex justify-between items-center'>
+                <div>
+                  <p className='font-semibold text-2xl'>₹{laptop.price/10}</p>
+                  <p><s className='text-gray-600 mr-1'>₹{laptop.price}</s><span className='text-green-500 font-bold'>10% off</span></p>
+                </div>
+                <button type="button" className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br  shadow-lg shadow-blue-500/50  font-medium rounded-lg text-sm px-5 py-2.5 text-center">Buy now</button>
+              </div>
             </div>
           </div>
         )

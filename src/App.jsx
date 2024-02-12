c02c98cbb4994ad0a2f67f9c263745b045a89faa
ashishverma2831 <ashishverma510207@gmail.com>
2024-02-12 20:11:47 +0530

    Refactor brand selection in App component

--- src/App.jsx
+++ src/App.jsx
@@ -49,42 +49,60 @@ const App = () => {
   }
 
 
-  const prices = [
-    "Below 20,000",
-    "Above 20,000 & Below 30,000",
-    "Above 30,000 & Below 40,000",
-    "Above 40,000 & Below 50,000",
-    "Above 50,000 & Below 60,000",
-    "Above 60,000 & Below 70,000",
-    "Above 70,000 & Below 80,000",
-    "Above 80,000"
-  ];
+  // const prices = [
+  //   "Below 20,000",
+  //   "Above 20,000 & Below 30,000",
+  //   "Above 30,000 & Below 40,000",
+  //   "Above 40,000 & Below 50,000",
+  //   "Above 50,000 & Below 60,000",
+  //   "Above 60,000 & Below 70,000",
+  //   "Above 70,000 & Below 80,000",
+  //   "Above 80,000"
+  // ];
 
-  const [selectedPrice, setSelectedPrice] = useState([]);
+  const brands = [
+    "HP",
+    "Asus",
+    "Lenevo",
+    "Dell",
+    "MSI",
+    "Apple",
+    "Samsung",
+    "Zebronics",
+    "Microsoft",
+    "Honor",
+    "Acer",
+    "Primebook"
+];
 
-  const handlePrice = (e, price) => {
+ 
+  const [selectedBrand, setSelectedBrand] = useState([]);
+
+  const handleBrand = (e, brand) => {
     console.log(e.target.checked);
-    if (selectedPrice.includes(price)) {
-      const filteredPrice = selectedPrice.filter((p)=>{return p!==price});
-      if(filteredPrice.length===0){
+    if (selectedBrand.includes(brand)) {
+      const filteredBrand = selectedBrand.filter((b)=>{return b!==brand});
+      if(filteredBrand.length===0){
         setProductList(laptops);
       }
       else{
         const filteredData = laptops.filter((laptop)=>{
-          return filteredPrice.includes(laptop.price);
+          return filteredBrand.includes(laptop.brand);
         })
         setProductList(filteredData)
+        // console.log(filteredData);
       }
+      setSelectedBrand(filteredBrand);
+      console.log(filteredBrand);
       }
     else {
-      const filteredPrice = [...selectedPrice, price];
+      const filteredBrand = [...selectedBrand, brand];
       const filteredData = laptops.filter((laptop) => {
-        return filteredPrice.includes(laptop.price)
+        return filteredBrand.includes(laptop.brand)
       })
-
-      setSelectedPrice(filteredPrice);
       setProductList(filteredData)
-      console.log([...selectedPrice, price]);
+      setSelectedBrand(filteredBrand);
+      console.log([...selectedBrand, brand]);
     }
   }
 
@@ -145,8 +163,7 @@ const App = () => {
         <div className=' col-start-1 col-span-4 m-4 border shadow bg-white px-4'>
           <div>
             <div id="accordion-collapse" data-accordion="collapse">
-              {/* <Price /> */}
-              <div>
+              {/* <div>
                 <h2 id="accordion-collapse-heading-25">
                   <button
                     type="button"
@@ -190,9 +207,54 @@ const App = () => {
                     })}
                   </div>
                 </div>
-              </div>
+              </div> */}
               <hr />
-              <Brand />
+              {/* <Brand /> */}
+              <div>
+        <h2 id="accordion-collapse-heading-3">
+                <button
+                    type="button"
+                    class="flex items-center justify-between w-full p-3 font-medium rtl:text-right text-gray-500 "
+                    data-accordion-target="#accordion-collapse-body-3"
+                    aria-expanded="true"
+                    aria-controls="accordion-collapse-body-3"
+                >
+                    <span>Brand</span>         
+                    <svg
+                        data-accordion-icon
+                        class="w-3 h-3 rotate-180 shrink-0"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 10 6"
+                    >
+                        <path
+                            stroke="currentColor"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M9 5 5 1 1 5"
+                        />
+                    </svg>
+                </button>
+            </h2>
+            <div
+                id="accordion-collapse-body-3"
+                class="hidden"
+                aria-labelledby="accordion-collapse-heading-3"
+            >
+                <div class="p-5 border border-b-0 border-gray-200 ">
+                    {brands.map((brand) => {
+                        return (
+                            <div class="flex items-center mb-4">
+                                <input id="default-checkbox" checked={selectedBrand.includes(brand)} onChange={(e)=>{handleBrand(e,brand)}} type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded"/>
+                                <label for="default-checkbox" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">{brand}</label>
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+    </div>
               <hr />
               <Type />
               <hr />
